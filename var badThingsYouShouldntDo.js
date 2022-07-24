c9562610d0bf56f0b456717619eebce6fe20ea7a
Console.log('im, MHAzcmF0MHI2NjY=') <106628238+0p3rat0r666@users.noreply.github.com>
2022-07-24 18:26:21 -0500

    Add files via upload

---
+++ var badThingsYouShouldntDo.js
@@ -0,0 +1,8 @@
+var badThingsYouShouldntDo = [
+    'drink piss',
+    'groom children',
+    'commit arson',
+    'do drugs',
+    'kill people'
+];
+console.log(badThingsYouShouldntDo + ' is bad, dont do it or youre stupid!');
